3831426d7f8dac97ef8d2d24d3f702644bfd8db8
HiganTGB <trangiabao16082003@gmail.com>
2025-05-09 03:17:38 +0700

    init

---
+++ GSVNBackEnd/src/test/java/com/tgb/gsvnbackend/GsvnBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tgb.gsvnbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GsvnBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
