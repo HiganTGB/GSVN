c1413f97043cbf896949e51291e6c9ebc5d746e9
HiganTGB <trangiabao16082003@gmail.com>
2025-05-09 10:51:08 +0700

    Add Order Entity

---
+++ GSVNBackEnd/src/main/java/com/tgb/gsvnbackend/model/entity/Order.java
@@ -0,0 +1,64 @@
+package com.tgb.gsvnbackend.model.entity;
+
+
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
+
+
+
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Map;
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(callSuper = true)
+@Data
+@Builder
+@Document(collection = "orders") // Ánh xạ tới collection có tên "orders" (bạn có thể đặt tên khác)
+public class Order extends AbstractMappedEntity {
+
+    @Id
+    private String id;
+    private String userId;
+    private String name;
+    private String note;
+    private String state;
+    private LocalDate date;
+    private List<LineItem> lineItems;
+    private BigDecimal sub_total;
+    @Data
+    public static class LineItem {
+        private String id;
+        private String sku;
+        private String name;
+        private Integer quantity;
+        private Pricing pricing;
+    }
+
+    @Data
+    public static class Pricing {
+        private BigDecimal retail;
+        private BigDecimal sale;
+
+    }
+    @Data
+    public static class ShippingAddress {
+        private String receiver;
+        private String street;
+        private String city;
+        private String state;
+        private String zip;
+        private String phone;
+    }
+    @Data
+    public static class PaymentMethod {
+        private String status;
+        private String number;
+    }
+}
---
+++ GSVNBackEnd/src/main/java/com/tgb/gsvnbackend/model/enumeration/State.java
@@ -0,0 +1,5 @@
+package com.tgb.gsvnbackend.model.enumeration;
+
+public enum State {
+    Saga,Pending,Processing,Delivery,Delivered,Cancel,Success,Reject
+}
