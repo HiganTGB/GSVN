91d680c5e7cdbece757d86f2d0dd4d0c48627bea
HiganTGB <trangiabao16082003@gmail.com>
2025-05-15 23:30:24 +0700

    Update GsvnBackEndApplication.java

--- GSVNBackEnd/src/main/java/com/tgb/gsvnbackend/GsvnBackEndApplication.java
+++ GSVNBackEnd/src/main/java/com/tgb/gsvnbackend/GsvnBackEndApplication.java
@@ -2,8 +2,12 @@ package com.tgb.gsvnbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableMongoRepositories(basePackages = "com.tgb.gsvnbackend.repository.mongoRepository")
+@EnableJpaRepositories(basePackages = "com.tgb.gsvnbackend.repository.jpaRepository")
 public class GsvnBackEndApplication {
 
     public static void main(String[] args) {
